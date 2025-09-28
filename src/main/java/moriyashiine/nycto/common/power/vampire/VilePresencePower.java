24d9929372cdb9007d961da8a2e8f825a1508ae7
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-09-28 15:25:15 -0500

    fix #2

--- src/main/java/moriyashiine/nycto/common/power/vampire/VilePresencePower.java
+++ src/main/java/moriyashiine/nycto/common/power/vampire/VilePresencePower.java
@@ -5,6 +5,8 @@ package moriyashiine.nycto.common.power.vampire;
 
 import moriyashiine.nycto.api.NyctoAPI;
 import moriyashiine.nycto.api.power.NegativePower;
+import moriyashiine.nycto.common.component.entity.power.vampire.VampiricThrallComponent;
+import moriyashiine.nycto.common.init.ModEntityComponents;
 import moriyashiine.nycto.common.init.ModPowers;
 import moriyashiine.nycto.common.init.ModStatusEffects;
 import moriyashiine.nycto.common.tag.ModEntityTypeTags;
@@ -15,17 +17,18 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
+import org.jetbrains.annotations.Nullable;
 
 public class VilePresencePower extends NegativePower {
 	@Override
 	public void tick(ServerPlayerEntity player) {
 		if (!player.isCreative()) {
-			player.getWorld().getEntitiesByType(EntityType.CAT, player.getBoundingBox().expand(16), VilePresencePower::isAffected).forEach(cat -> {
+			player.getWorld().getEntitiesByType(EntityType.CAT, player.getBoundingBox().expand(16), VilePresencePower::canBeAffected).forEach(cat -> {
 				if ((cat.age + cat.getId()) % 100 == 0) {
 					cat.hiss();
 				}
 			});
-			player.getWorld().getEntitiesByType(EntityType.WOLF, player.getBoundingBox().expand(16), VilePresencePower::isAffected).forEach(wolf -> {
+			player.getWorld().getEntitiesByType(EntityType.WOLF, player.getBoundingBox().expand(16), VilePresencePower::canBeAffected).forEach(wolf -> {
 				if ((wolf.age + wolf.getId()) % 100 == 0) {
 					wolf.chooseRandomAngerTime();
 				}
@@ -34,11 +37,11 @@ public class VilePresencePower extends NegativePower {
 	}
 
 	public static boolean isAffected(Entity entity, PlayerEntity player) {
-		return isAffected(entity) && shouldApply(player);
+		return canBeAffected(entity) && shouldApply(player);
 	}
 
 	public static boolean isAffected(Entity entity, double distance) {
-		if (isAffected(entity)) {
+		if (canBeAffected(entity)) {
 			for (PlayerEntity player : entity.getWorld().getPlayers()) {
 				if (player.distanceTo(entity) <= distance && shouldApply(player)) {
 					return true;
@@ -52,13 +55,17 @@ public class VilePresencePower extends NegativePower {
 		return NyctoUtil.isSurvival(player) && NyctoAPI.hasPower(player, ModPowers.VILE_PRESENCE);
 	}
 
-	private static boolean isAffected(Entity entity) {
+	private static boolean canBeAffected(Entity entity) {
 		if (entity.getType().isIn(ModEntityTypeTags.VILE_PRESENCE_IMMUNE)) {
 			return false;
 		}
 		if (entity instanceof LivingEntity living && living.hasStatusEffect(ModStatusEffects.HYPNOTIZED)) {
 			return false;
 		}
-		return !(entity instanceof TameableEntity tameable) || !tameable.isTamed();
+		if (entity instanceof TameableEntity tameable && tameable.isTamed()) {
+			return false;
+		}
+		@Nullable VampiricThrallComponent vampiricThrallComponent = ModEntityComponents.VAMPIRIC_THRALL.getNullable(entity);
+		return vampiricThrallComponent == null || !vampiricThrallComponent.isThralled();
 	}
 }
