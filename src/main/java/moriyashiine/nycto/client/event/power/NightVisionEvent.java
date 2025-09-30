bc17ad6588bb1eb12fb0a39d67b52b353ce8c29d
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-09-30 10:26:00 -0500

    fix crash when spectating non-players

--- src/main/java/moriyashiine/nycto/client/event/power/NightVisionEvent.java
+++ src/main/java/moriyashiine/nycto/client/event/power/NightVisionEvent.java
@@ -3,13 +3,19 @@
  */
 package moriyashiine.nycto.client.event.power;
 
+import moriyashiine.nycto.common.component.entity.power.NightVisionComponent;
 import moriyashiine.nycto.common.init.ModEntityComponents;
 import moriyashiine.strawberrylib.api.event.client.ModifyNightVisionStrengthEvent;
 import net.minecraft.entity.LivingEntity;
+import org.jetbrains.annotations.Nullable;
 
 public class NightVisionEvent implements ModifyNightVisionStrengthEvent {
 	@Override
-	public float modify(float strength, LivingEntity player) {
-		return ModEntityComponents.NIGHT_VISION.get(player).getStrengthPercentage();
+	public float modify(float strength, LivingEntity entity) {
+		@Nullable NightVisionComponent nightVisionComponent = ModEntityComponents.NIGHT_VISION.getNullable(entity);
+		if (nightVisionComponent != null) {
+			return nightVisionComponent.getStrengthPercentage();
+		}
+		return 0;
 	}
 }
