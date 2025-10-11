78da42ecf9cce39228ac4fd70653efaa1b95397a
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-10-11 13:12:27 -0500

    hunters can no longer be disarmed with enchancement

--- src/main/java/moriyashiine/nycto/data/provider/ModEntityTypeTagProvider.java
+++ src/main/java/moriyashiine/nycto/data/provider/ModEntityTypeTagProvider.java
@@ -9,8 +9,11 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.fabricmc.fabric.api.tag.convention.v2.ConventionalEntityTypeTags;
 import net.minecraft.entity.EntityType;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.EntityTypeTags;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -81,5 +84,8 @@ public class ModEntityTypeTagProvider extends FabricTagProvider.EntityTypeTagPro
 				.add(EntityType.WARDEN);
 		valueLookupBuilder(ModEntityTypeTags.VILE_PRESENCE_IMMUNE)
 				.forceAddTag(EntityTypeTags.UNDEAD);
+
+		valueLookupBuilder(TagKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of("enchancement", "cannot_disarm")))
+				.add(ModEntityTypes.HUNTER);
 	}
 }
