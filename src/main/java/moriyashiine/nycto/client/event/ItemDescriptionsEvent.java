c10a4527863896c604d5edc8032141f6516a7602
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-11-05 14:16:37 -0600

    remove descriptions from vampiric dagger and garlic brew

--- src/main/java/moriyashiine/nycto/client/event/ItemDescriptionsEvent.java
+++ src/main/java/moriyashiine/nycto/client/event/ItemDescriptionsEvent.java
@@ -4,19 +4,12 @@
 package moriyashiine.nycto.client.event;
 
 import moriyashiine.nycto.api.NyctoAPI;
-import moriyashiine.nycto.common.init.ModComponentTypes;
-import moriyashiine.nycto.common.init.ModPotions;
-import moriyashiine.nycto.common.item.VampiricDaggerItem;
 import moriyashiine.nycto.common.tag.ModItemTags;
 import moriyashiine.nycto.common.util.NyctoUtil;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.PotionContentsComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.screen.ScreenTexts;
@@ -25,7 +18,6 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
 import java.util.*;
-import java.util.function.Predicate;
 
 public class ItemDescriptionsEvent implements ItemTooltipCallback {
 	public static final Set<TooltipAdder> ARMOR_SET_BONUSES = new HashSet<>();
@@ -37,20 +29,7 @@ public class ItemDescriptionsEvent implements ItemTooltipCallback {
 		ARMOR_SET_BONUSES.add(stack -> getArmorSetText(stack, ModItemTags.VAMPIRE_HUNTER_ARMOR, ItemDescriptionsEvent.VAMPIRE_HUNTER_ARMOR_BONUSES));
 	}
 
-	private static final Text HOLD_LEFT_SHEFT_TEXT = Text.translatable("tooltip.nycto.hold_left_shift", Text.translatable("key.keyboard.left.shift").formatted(Formatting.GOLD)).formatted(Formatting.GRAY);
-	private static final Text HOLDING_PLAYER_BLOOD_TEXT = Text.translatable("tooltip.nycto.holding_player_blood").formatted(Formatting.ITALIC, Formatting.GRAY);
-	private static final Text HOLDING_VAMPIRE_BLOOD_TEXT = Text.translatable("tooltip.nycto.holding_vampire_blood").formatted(Formatting.ITALIC, Formatting.GRAY);
-
 	private static final Text WEAKNESS_TEXT = Text.translatable("tooltip.nycto.in_weakness_tag").formatted(Formatting.ITALIC, Formatting.RED);
-
-	private static final List<Text> VAMPIRIC_DAGGER_DESCRIPTIONS = List.of(
-			Text.translatable("tooltip.nycto.vampiric_dagger.desc.0").formatted(Formatting.GRAY),
-			Text.translatable("tooltip.nycto.vampiric_dagger.desc.1").formatted(Formatting.GRAY)
-	);
-	private static final List<Text> GARLIC_BREW_DESCRIPTIONS = List.of(
-			Text.translatable("tooltip.nycto.garlic_brew.desc.0").formatted(Formatting.GRAY),
-			Text.translatable("tooltip.nycto.garlic_brew.desc.1").formatted(Formatting.GRAY)
-	);
 
 	private static final Text ARMOR_SET_DESCRIPTION = Text.translatable("tooltip.nycto.armor_set.desc").formatted(Formatting.GOLD);
 	private static final List<Text> VAMPIRE_ARMOR_BONUSES = List.of(
@@ -68,8 +47,6 @@ public class ItemDescriptionsEvent implements ItemTooltipCallback {
 
 	@Override
 	public void getTooltip(ItemStack stack, Item.TooltipContext tooltipContext, TooltipType tooltipType, List<Text> lines) {
-		addTooltip(lines, getVampiricDaggerText(stack));
-		addTooltip(lines, getDescriptionText(stack, GARLIC_BREW_DESCRIPTIONS, s -> s.isOf(Items.POTION) && s.getOrDefault(DataComponentTypes.POTION_CONTENTS, PotionContentsComponent.DEFAULT).potion().stream().anyMatch(potion -> potion == ModPotions.GARLIC || potion == ModPotions.LONG_GARLIC || potion == ModPotions.STRONG_GARLIC)));
 		addTooltip(lines, getWeaknessText(stack));
 	}
 
@@ -91,38 +68,6 @@ public class ItemDescriptionsEvent implements ItemTooltipCallback {
 				}
 				lines.add(bonus);
 			}
-			return Optional.of(lines);
-		}
-		return Optional.empty();
-	}
-
-	private static Optional<List<Text>> getVampiricDaggerText(ItemStack stack) {
-		Optional<List<Text>> description = getDescriptionText(stack, VAMPIRIC_DAGGER_DESCRIPTIONS, s -> s.getItem() instanceof VampiricDaggerItem);
-		description.ifPresent(lines -> {
-			boolean playerBlood = stack.getOrDefault(ModComponentTypes.PLAYER_BLOOD, false);
-			boolean vampireBlood = stack.getOrDefault(ModComponentTypes.VAMPIRE_BLOOD, false);
-			if (!playerBlood && !vampireBlood) {
-				lines.removeLast();
-			}
-			if (playerBlood) {
-				lines.add(HOLDING_PLAYER_BLOOD_TEXT);
-			}
-			if (vampireBlood) {
-				lines.add(HOLDING_VAMPIRE_BLOOD_TEXT);
-			}
-		});
-		return description;
-	}
-
-	private static Optional<List<Text>> getDescriptionText(ItemStack stack, List<Text> descriptions, Predicate<ItemStack> predicate) {
-		if (predicate.test(stack)) {
-			List<Text> lines = new ArrayList<>();
-			if (InputUtil.isKeyPressed(client.getWindow(), InputUtil.GLFW_KEY_LEFT_SHIFT)) {
-				lines.addAll(descriptions);
-			} else {
-				lines.add(HOLD_LEFT_SHEFT_TEXT);
-			}
-			lines.add(ScreenTexts.EMPTY);
 			return Optional.of(lines);
 		}
 		return Optional.empty();
--- src/main/java/moriyashiine/nycto/common/item/VampiricDaggerItem.java
+++ src/main/java/moriyashiine/nycto/common/item/VampiricDaggerItem.java
@@ -9,6 +9,7 @@ import moriyashiine.nycto.common.init.ModSoundEvents;
 import moriyashiine.nycto.common.recipe.BloodExtractionRecipe;
 import moriyashiine.strawberrylib.api.objects.records.ModifierTrio;
 import net.minecraft.component.type.AttributeModifierSlot;
+import net.minecraft.component.type.TooltipDisplayComponent;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
@@ -17,13 +18,18 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ClickType;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+
+import java.util.function.Consumer;
 
 import static moriyashiine.strawberrylib.api.module.SLibRegistries.editModifiers;
 
@@ -32,6 +38,9 @@ public class VampiricDaggerItem extends Item {
 
 	private static final EntityAttributeModifier ENTITY_INTERACTION_RANGE_MODIFIER = new EntityAttributeModifier(Nycto.id("vampiric_dagger_entity_interaction_range"), -0.5, EntityAttributeModifier.Operation.ADD_VALUE);
 	private static final ModifierTrio MODIFIER = new ModifierTrio(EntityAttributes.ENTITY_INTERACTION_RANGE, ENTITY_INTERACTION_RANGE_MODIFIER, AttributeModifierSlot.MAINHAND);
+
+	private static final Text HOLDING_PLAYER_BLOOD_TEXT = Text.translatable("tooltip.nycto.holding_player_blood").formatted(Formatting.ITALIC, Formatting.GRAY);
+	private static final Text HOLDING_VAMPIRE_BLOOD_TEXT = Text.translatable("tooltip.nycto.holding_vampire_blood").formatted(Formatting.ITALIC, Formatting.GRAY);
 
 	public VampiricDaggerItem(Settings settings) {
 		super(editModifiers(() -> settings.sword(ToolMaterial.IRON, 2, -2), MODIFIER));
@@ -72,6 +81,16 @@ public class VampiricDaggerItem extends Item {
 			return true;
 		}
 		return super.onClicked(stack, otherStack, slot, clickType, player, cursorStackReference);
+	}
+
+	@Override
+	public void appendTooltip(ItemStack stack, TooltipContext context, TooltipDisplayComponent displayComponent, Consumer<Text> textConsumer, TooltipType type) {
+		if (stack.getOrDefault(ModComponentTypes.PLAYER_BLOOD, false)) {
+			textConsumer.accept(HOLDING_PLAYER_BLOOD_TEXT);
+		}
+		if (stack.getOrDefault(ModComponentTypes.VAMPIRE_BLOOD, false)) {
+			textConsumer.accept(HOLDING_VAMPIRE_BLOOD_TEXT);
+		}
 	}
 
 	@Override
