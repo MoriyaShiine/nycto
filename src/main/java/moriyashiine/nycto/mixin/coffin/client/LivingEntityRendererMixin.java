c16e66489bf1e7eaad85e45efb562b67c51d80c1
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-11-21 16:22:49 -0600

    update render mixin priorities

--- src/main/java/moriyashiine/nycto/mixin/coffin/client/LivingEntityRendererMixin.java
+++ src/main/java/moriyashiine/nycto/mixin/coffin/client/LivingEntityRendererMixin.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = LivingEntityRenderer.class, priority = 999)
+@Mixin(value = LivingEntityRenderer.class, priority = 500)
 public class LivingEntityRendererMixin<T extends LivingEntity, S extends LivingEntityRenderState> {
 	@Inject(method = "render(Lnet/minecraft/client/render/entity/state/LivingEntityRenderState;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/command/OrderedRenderCommandQueue;Lnet/minecraft/client/render/state/CameraRenderState;)V", at = @At("HEAD"), cancellable = true)
 	private void nycto$coffin(S renderState, MatrixStack matrices, OrderedRenderCommandQueue queue, CameraRenderState cameraRenderState, CallbackInfo ci) {
--- src/main/java/moriyashiine/nycto/mixin/vampire/client/InGameHudMixin.java
+++ src/main/java/moriyashiine/nycto/mixin/vampire/client/InGameHudMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = InGameHud.class, priority = 999)
+@Mixin(value = InGameHud.class, priority = 500)
 public class InGameHudMixin {
 	@Inject(method = "renderFood", at = @At("HEAD"), cancellable = true)
 	private void nycto$vampire(DrawContext context, PlayerEntity player, int top, int right, CallbackInfo ci) {
