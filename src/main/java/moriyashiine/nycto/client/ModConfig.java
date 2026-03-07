ffe550e5c0908fd8eac5f1e3370f3ff8c97b34b8
marcohugo1202-prog <marcohugo1202@gmail.com>
2026-03-07 18:56:53 +0100

    Add configuration options for more vampire balance

--- src/main/java/moriyashiine/nycto/client/ModConfig.java
+++ src/main/java/moriyashiine/nycto/client/ModConfig.java
@@ -10,4 +10,28 @@ public class ModConfig extends MidnightConfig {
 	public static boolean vampireChargeJump = true;
 	@Entry(category = "client")
 	public static boolean vampireStepHeight = true;
+
+	// Hunter balance
+	@Entry(category = "server", min = 1, max = 20)
+	public static int hunterMaxHeat = 5;
+	@Entry(category = "server", min = 0, max = 20)
+	public static int hunterMaxSpawns = 3;
+	@Entry(category = "server")
+	public static boolean weaponsBypassBloodVeil = true;
+	@Entry(category = "server")
+	public static boolean weaponsHaltRegeneration = true;
+
+	// Sun balance
+	@Entry(category = "server")
+	public static boolean sunDamageEnabled = true;
+	@Entry(category = "server")
+	public static boolean sunBypassesBloodVeil = true;
+	@Entry(category = "server")
+	public static boolean sunHaltsRegeneration = true;
+
+	// Fire balance
+	@Entry(category = "server")
+	public static boolean fireBypassesBloodVeil = true;
+	@Entry(category = "server")
+	public static boolean fireHaltsRegeneration = true;
 }
