4b45732594521c7ef76de8bd115f037314e3cdfe
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-04-20 13:44:32 -0500

    fix bat form not being able to swing their arm

--- src/main/java/moriyashiine/nycto/mixin/power/vampire/batform/client/LocalPlayerMixin.java
+++ src/main/java/moriyashiine/nycto/mixin/power/vampire/batform/client/LocalPlayerMixin.java
@@ -4,6 +4,7 @@
 
 package moriyashiine.nycto.mixin.power.vampire.batform.client;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import moriyashiine.nycto.common.init.ModEntityComponents;
 import net.minecraft.client.player.LocalPlayer;
@@ -15,5 +16,10 @@ public class LocalPlayerMixin {
 	@ModifyReturnValue(method = "isSprintingPossible", at = @At("RETURN"))
 	private boolean nycto$batForm(boolean original) {
 		return original && !ModEntityComponents.BAT_FORM.get(this).isEnabled();
+	}
+
+	@ModifyExpressionValue(method = "aiStep", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;isSpectator()Z"))
+	private boolean nycto$batFormFly(boolean original) {
+		return original || ModEntityComponents.BAT_FORM.get(this).isEnabled();
 	}
 }
