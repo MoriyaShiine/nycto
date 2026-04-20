4048b3487aa7ef8990ab03beb7652e54c55576a8
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-04-20 12:05:38 -0500

    bat form now reduces detection range of mobs

--- src/main/java/moriyashiine/nycto/mixin/power/vampire/batform/LivingEntityMixin.java
+++ src/main/java/moriyashiine/nycto/mixin/power/vampire/batform/LivingEntityMixin.java
@@ -5,9 +5,12 @@
 package moriyashiine.nycto.mixin.power.vampire.batform;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import moriyashiine.nycto.common.init.ModEntityComponents;
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalEntityTypeTags;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,5 +21,14 @@ public class LivingEntityMixin {
 	@ModifyExpressionValue(method = "hurtServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/damagesource/DamageSource;is(Lnet/minecraft/tags/TagKey;)Z", ordinal = 5))
 	private boolean nycto$batForm(boolean original, ServerLevel level, DamageSource source) {
 		return original || (source.getDirectEntity() instanceof Player player && ModEntityComponents.BAT_FORM.get(player).isEnabled());
+	}
+
+	@SuppressWarnings("ConstantValue")
+	@ModifyReturnValue(method = "getVisibilityPercent", at = @At("RETURN"))
+	private double nycto$batForm(double original, Entity targetingEntity) {
+		if (targetingEntity != null && !targetingEntity.is(ConventionalEntityTypeTags.BOSSES) && (Object) this instanceof Player player && ModEntityComponents.BAT_FORM.get(player).isEnabled()) {
+			return original / 2;
+		}
+		return original;
 	}
 }
