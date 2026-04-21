f6a090905e64968ad56c3df8e57338124a41f16d
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-04-21 11:52:26 -0500

    fix cases of fire animation not clearing when it should

--- src/main/java/moriyashiine/nycto/mixin/sunexposure/EntityMixin.java
+++ src/main/java/moriyashiine/nycto/mixin/sunexposure/EntityMixin.java
@@ -6,23 +6,32 @@ package moriyashiine.nycto.mixin.sunexposure;
 
 import moriyashiine.nycto.api.NyctoAPI;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@SuppressWarnings("ConstantValue")
 @Mixin(Entity.class)
-public class EntityMixin {
+public abstract class EntityMixin {
+	@Shadow
+	private Level level;
+
+	@Shadow
+	public abstract boolean isAlive();
+
 	@Inject(method = "clearFire", at = @At("HEAD"), cancellable = true)
 	private void nycto$sunExposure(CallbackInfo ci) {
-		if (NyctoAPI.isSunExposed((Entity) (Object) this)) {
+		if (!level.isClientSide() && isAlive() && NyctoAPI.isSunExposed((Entity) (Object) this)) {
 			ci.cancel();
 		}
 	}
 
 	@Inject(method = "playEntityOnFireExtinguishedSound", at = @At("HEAD"), cancellable = true)
 	private void nycto$sunExposureSound(CallbackInfo ci) {
-		if (NyctoAPI.isSunExposed((Entity) (Object) this)) {
+		if (isAlive() && NyctoAPI.isSunExposed((Entity) (Object) this)) {
 			ci.cancel();
 		}
 	}
