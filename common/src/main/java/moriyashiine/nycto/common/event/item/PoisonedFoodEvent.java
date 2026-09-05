72bde92c0d43a7ba35e023a734297bea2bdede47
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-09-05 18:59:41 -0500

    use new slib food events

--- common/src/main/java/moriyashiine/nycto/common/event/item/PoisonedFoodEvent.java
+++ common/src/main/java/moriyashiine/nycto/common/event/item/PoisonedFoodEvent.java
@@ -3,22 +3,21 @@ package moriyashiine.nycto.common.event.item;
 import moriyashiine.nycto.api.NyctoAPI;
 import moriyashiine.nycto.common.init.NyctoDamageTypes;
 import moriyashiine.nycto.common.init.NyctoDataComponents;
-import moriyashiine.strawberrylib.api.event.EatFoodEvent;
+import moriyashiine.strawberrylib.api.event.FoodEvents;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
-public class PoisonedFoodEvent implements EatFoodEvent {
+public class PoisonedFoodEvent implements FoodEvents.Eat {
 	public static void init() {
-		EatFoodEvent.EVENT.register(new PoisonedFoodEvent());
+		FoodEvents.EAT.register(new PoisonedFoodEvent());
 	}
 
 	@Override
-	public void eat(Level level, LivingEntity user, ItemStack stack, FoodProperties properties) {
+	public void eat(Level level, LivingEntity user, ItemStack stack) {
 		if (level instanceof ServerLevel serverLevel && stack.getOrDefault(NyctoDataComponents.POISONED, false)) {
 			user.addEffect(new MobEffectInstance(MobEffects.POISON, 400, 1));
 			if (NyctoAPI.isWerewolf(user)) {
